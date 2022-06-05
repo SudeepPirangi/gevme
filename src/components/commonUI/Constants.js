f70a670e26603aa648629a64ee1cc1fee9c8a48e
SudeepPirangi <sudeep_edu@yahoo.com>
2022-06-05 10:19:31 +0530

    Deletes commonUI folder

--- src/components/commonUI/Constants.js
+++
@@ -1,2 +0,0 @@
-export const PROFILE = "PROFILE";
-export const POSTS = "POSTS";
--- src/components/commonUI/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Loader } from 'rsuite';
-
-const Loading = () => {
-  return <Loader content="Loading..." />
-};
-
-export default Loading;
